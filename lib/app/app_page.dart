850e61c8c94a368717f41885c107e48f25e03c97
Ibrahim Adeosun <ibrahim.adeosun.ia@gmail.com>
2025-12-14 06:25:25 -0500

    Created page to show when not connected to the internet

--- lib/app/app_page.dart
+++ lib/app/app_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:routefly/routefly.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 import 'app_widget.dart';
 
@@ -16,10 +17,18 @@ class AppPage extends StatefulWidget {
 class _AppPageState extends State<AppPage> {
   late final StreamSubscription<User?> _sub;
   bool _isLoading = true;
+  bool? isConnected;
+  StreamSubscription<InternetStatus>? _connectedSub;
 
   @override
   void initState() {
     super.initState();
+
+    _connectedSub = InternetConnection().onStatusChange.listen((status) {
+      setState(() {
+        isConnected = status == InternetStatus.connected;
+      });
+    });
 
     _sub = FirebaseAuth.instance.authStateChanges().listen(
       (user) {
@@ -50,15 +59,41 @@ class _AppPageState extends State<AppPage> {
   @override
   void dispose() {
     _sub.cancel();
+    _connectedSub?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (isConnected != true) {
+      return const OfflineScreen();
+    }
+
     if (_isLoading) {
       return const Center(child: CircularProgressIndicator());
     }
 
     return Center(child: Text("You are logged in"));
+  }
+}
+
+class OfflineScreen extends StatelessWidget {
+  const OfflineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFF9F9F9),
+      body: Center(
+        child: Text(
+          'You need to connect to the internet.',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF333333),
+          ),
+        ),
+      ),
+    );
   }
 }
