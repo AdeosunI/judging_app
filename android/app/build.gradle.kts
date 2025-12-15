4bf25d7de1fe67431c1c019fb103970cca8e63e8
Ibrahim Adeosun <ibrahim.adeosun.ia@gmail.com>
2025-12-14 21:30:14 -0500

    renamed the project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.judging_app"
+        applicationId = "com.yourcompany.quadconnect"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
